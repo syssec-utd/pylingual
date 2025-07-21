e0cfc70147a29ee2271f9eb61b35bad7c4d64606
caandt <caandt@twoha.cc>
2025-07-21 16:47:37 -0500

    format

--- test/Exception.py
+++ test/Exception.py
@@ -4,12 +4,14 @@ def a0_bare_try_except():
     except:
         print(2)
 
+
 def a1_bare_try_except_fallthrough():
     try:
         print(1)
     except:
         print(2)
     print(3)
+
 
 # 3.11/3.12/3.13 Duplicate blocks causing blocks to not match
 def b0_nested_try_except():
@@ -22,6 +24,7 @@ def b0_nested_try_except():
         except:
             print(4)
 
+
 def b1_nested_try_except_fallthrough():
     try:
         print(1)
@@ -33,7 +36,8 @@ def b1_nested_try_except_fallthrough():
             print(4)
         print(5)
 
-# 3.13 Duplicate blocks 
+
+# 3.13 Duplicate blocks
 def b2_nested_try_except_early_fallthrough():
     try:
         print(1)
@@ -44,6 +48,7 @@ def b2_nested_try_except_early_fallthrough():
         except:
             print(4)
     print(5)
+
 
 def b3_nested_try_except_double_fallthrough():
     try:
@@ -56,6 +61,7 @@ def b3_nested_try_except_double_fallthrough():
             print(4)
         print(5)
     print(6)
+
 
 # 3.11/3.12/3.13 Duplicate blocks causing blocks to not match
 def c0_multi_except_nested():
@@ -73,6 +79,7 @@ def c0_multi_except_nested():
             print(6)
         except:
             print(7)
+
 
 # 3.11/3.12/3.13 Duplicate blocks causing blocks to not match
 def c1_multi_except_nested_fallthrough():
@@ -92,6 +99,7 @@ def c1_multi_except_nested_fallthrough():
         except:
             print(8)
 
+
 def c2_multi_except_nested_fallthrough2():
     try:
         print(1)
@@ -110,7 +118,8 @@ def c2_multi_except_nested_fallthrough2():
             print(8)
         print(9)
 
-# 3.13 Duplicate blocks 
+
+# 3.13 Duplicate blocks
 def c3_multi_except_nested_early_fallthrough():
     try:
         print(1)
@@ -127,6 +136,7 @@ def c3_multi_except_nested_early_fallthrough():
         except:
             print(7)
     print(8)
+
 
 def c4_multi_except_nested_all_fallthrough():
     try:
@@ -147,6 +157,7 @@ def c4_multi_except_nested_all_fallthrough():
         print(9)
     print(10)
 
+
 # 3.10/3.11/3.12/3.13 Duplicate blocks causing templates to not match
 # Discussed in issue 41
 def d0_named_except_nested():
@@ -159,6 +170,7 @@ def d0_named_except_nested():
         except:
             print(4)
 
+
 def d1_named_except_nested_fallthrough():
     try:
         print(1)
@@ -170,7 +182,8 @@ def d1_named_except_nested_fallthrough():
             print(4)
         print(5)
 
-# 3.13 Duplicate blocks 
+
+# 3.13 Duplicate blocks
 def d2_named_except_nested_early_fallthrough():
     try:
         print(1)
@@ -181,6 +194,7 @@ def d2_named_except_nested_early_fallthrough():
         except:
             print(4)
     print(5)
+
 
 def d3_named_except_nested_double_fallthrough():
     try:
@@ -194,6 +208,7 @@ def d3_named_except_nested_double_fallthrough():
         print(5)
     print(6)
 
+
 def e0_try_except_else():
     try:
         print(1)
@@ -202,6 +217,7 @@ def e0_try_except_else():
     else:
         print(3)
     print(4)
+
 
 def f0_try_except_else_finally():
     try:
@@ -214,6 +230,7 @@ def f0_try_except_else_finally():
         print(4)
     print(5)
 
+
 def g0_multi_except_with_else():
     try:
         print(1)
@@ -224,6 +241,7 @@ def g0_multi_except_with_else():
     else:
         print(4)
     print(5)
+
 
 def h0_multi_except_fallback_with_else():
     try:
@@ -238,6 +256,7 @@ def h0_multi_except_fallback_with_else():
         print(5)
     print(6)
 
+
 def i0_mixed_named_unnamed_except_with_else():
     try:
         print(1)
@@ -251,6 +270,7 @@ def i0_mixed_named_unnamed_except_with_else():
         print(5)
     print(6)
 
+
 def j0_named_except_with_else():
     try:
         print(1)
@@ -262,6 +282,7 @@ def j0_named_except_with_else():
         print(4)
     print(5)
 
+
 def k0_try_except_finally():
     try:
         print(1)
@@ -271,6 +292,7 @@ def k0_try_except_finally():
         print(3)
     print(4)
 
+
 def l0_specific_except_finally():
     try:
         print(1)
@@ -279,6 +301,7 @@ def l0_specific_except_finally():
     finally:
         print(3)
     print(4)
+
 
 def m0_multi_except():
     try:
@@ -291,6 +314,7 @@ def m0_multi_except():
         print(4)
     print(5)
 
+
 def n0_multi_except_with_fallback():
     try:
         print(1)
@@ -301,6 +325,7 @@ def n0_multi_except_with_fallback():
     except:
         print(4)
     print(5)
+
 
 def o0_multi_except_fallback_finally():
     try:
@@ -313,6 +338,7 @@ def o0_multi_except_fallback_finally():
         print(4)
     print(5)
 
+
 def p0_multi_named_except():
     try:
         print(1)
@@ -324,6 +350,7 @@ def p0_multi_named_except():
         print(4)
     print(5)
 
+
 def q0_mixed_named_unnamed_except():
     try:
         print(1)
@@ -334,6 +361,7 @@ def q0_mixed_named_unnamed_except():
     except:
         print(4)
     print(5)
+
 
 def r0_mixed_named_unnamed_except_finally():
     try:
@@ -348,6 +376,7 @@ def r0_mixed_named_unnamed_except_finally():
         print(5)
     print(6)
 
+
 def s0_named_except_fallback():
     try:
         print(1)
@@ -356,6 +385,7 @@ def s0_named_except_fallback():
     except:
         print(3)
     print(4)
+
 
 def t0_named_except_fallback_finally():
     try:
@@ -368,6 +398,7 @@ def t0_named_except_fallback_finally():
         print(4)
     print(5)
 
+
 def u0_multi_named_except_finally():
     try:
         print(1)
@@ -378,6 +409,7 @@ def u0_multi_named_except_finally():
     finally:
         print(4)
     print(5)
+
 
 def v0_multi_except_finally():
     try:
@@ -390,12 +422,14 @@ def v0_multi_except_finally():
         print(4)
     print(5)
 
+
 def w0_try_except_raise():
     try:
         print(1)
     except:
         print(2)
         raise Exc
+
 
 def x0_multi_except_raise():
     try:
@@ -407,12 +441,14 @@ def x0_multi_except_raise():
         print(3)
         raise Exc
 
+
 def y0_named_except_raise():
     try:
         print(1)
     except A as a:
         print(2)
         raise Exc
+
 
 # 3.11 Try return getting left outside of TryExcept
 def z0_try_except_return():
@@ -422,6 +458,7 @@ def z0_try_except_return():
     except:
         print(2)
 
+
 # 3.11 Try return getting left outside of TryExcept
 def z1_try_except_return_both():
     try:
@@ -430,6 +467,7 @@ def z1_try_except_return_both():
     except:
         print(2)
         return 3
+
 
 # 3.11 Try return getting left outside of TryExcept
 def aa0_multi_except_return():
@@ -441,7 +479,8 @@ def aa0_multi_except_return():
     except b:
         print(3)
 
-# 3.6/3.7/3.8 ExceptExc abandons tail node. 
+
+# 3.6/3.7/3.8 ExceptExc abandons tail node.
 # Could be fixed (?) but breaks other test cases
 # 3.11 Try return getting left outside of TryExcept
 def aa1_multi_except_return_both():
@@ -453,6 +492,7 @@ def aa1_multi_except_return_both():
         return 3
     except b:
         print(3)
+
 
 # 3.11 Try return getting left outside of TryExcept
 def ab0_named_except_raise_return():
@@ -466,6 +506,7 @@ def ab0_named_except_raise_return():
         print(3)
         raise Exc
 
+
 # 3.8 Double natural edge graph error (?)
 # 3.11 Try return getting left outside of TryExcept
 def ab1_named_except_return():
@@ -476,11 +517,13 @@ def ab1_named_except_return():
         print(2)
         return 3
 
+
 def ab2_named_except_return():
     try:
         return a
     except Exception as a:
         return b
+
 
 # 3.11/3.12/3.13 No template match
 def ac0_empty_try_finally():
@@ -488,6 +531,7 @@ def ac0_empty_try_finally():
         pass
     finally:
         print(1)
+
 
 def ad0_multiple_try_blocks():
     try:
@@ -500,6 +544,7 @@ def ad0_multiple_try_blocks():
     except:
         print(4)
 
+
 def ae0_try_except_else_nested_try():
     try:
         print(1)
@@ -510,6 +555,7 @@ def ae0_try_except_else_nested_try():
             print(3)
         except:
             print(4)
+
 
 # 3.9 Duplicate blocks (?)
 # 3.11/3.12/3.13 Matching priority TryElse TryFinally (?)
@@ -522,11 +568,13 @@ def af0_try_finally_nested_except():
         except:
             print(3)
 
+
 def ag0_try_except_tuple():
     try:
         print(1)
     except (A, B):
         print(2)
+
 
 # 3.9 Difficult template ambiguity between Try/TryFinally
 # 3.11/3.12/3.13 Matching priority TryElse TryFinally (?)
@@ -536,12 +584,14 @@ def ah0_try_finally_return():
     finally:
         return 2
 
+
 # 3.11/3.12/3.13 No template match (?)
 def ai0_try_return_finally():
     try:
         return 1
     finally:
         print(2)
+
 
 # 3.9/3.10 Difficult template ambiguity between Try/TryFinally
 # 3.11/3.12/3.13 No template match (?)
@@ -551,12 +601,14 @@ def aj0_try_return_finally_return():
     finally:
         return 2
 
+
 def ak0_try_except_raise_return():
     try:
         print(1)
         return 2
     except:
         raise Exception()
+
 
 # 3.8/3.9/3.10 No template match
 def al0_try_except_return_finally():
@@ -568,6 +620,7 @@ def al0_try_except_return_finally():
     finally:
         print(3)
 
+
 # 3.8/3.9/3.10 No template match
 # 3.11/3.12/3.13 Matching priority TryElse TryFinally (?)
 def am0_try_finally_raise():
@@ -577,12 +630,14 @@ def am0_try_finally_raise():
     finally:
         raise Exception()
 
+
 def an0_try_finally_fallthrough():
     try:
         print(1)
     finally:
         print(2)
     print(3)
+
 
 def ao0_try_finally_simple():
     try:
