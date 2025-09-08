7978a439e9e2f7fcb39d5a6e3348b9a9c32abd43
Xinlong Hu <xinlonghucs@gmail.com>
2025-09-08 13:47:12 -0500

    Conditional adjustments

--- pylingual/control_flow_reconstruction/templates/Conditional.py
+++ pylingual/control_flow_reconstruction/templates/Conditional.py
@@ -1,5 +1,5 @@
 from ..cft import ControlFlowTemplate, EdgeKind, register_template
-from ..utils import T, N, defer_source_to, has_some_lines, run_is, has_no_lines, with_instructions, exact_instructions, has_instval, starting_instructions, to_indented_source, make_try_match, without_top_level_instructions
+from ..utils import T, N, defer_source_to, has_some_lines, run_is, has_no_lines, with_instructions, exact_instructions, has_instval, starting_instructions, to_indented_source, make_try_match, without_top_level_instructions, ending_instructions
 from .Loop import BreakTemplate, ContinueTemplate
 
 
@@ -74,7 +74,7 @@ class IfElseLoop(ControlFlowTemplate):
     template = T(
         if_header=~N("else_body", "if_body").with_cond(without_top_level_instructions("WITH_EXCEPT_START", "CHECK_EXC_MATCH", "FOR_ITER")),
         if_body=~N("tail.").with_in_deg(1),
-        else_body=~N("tail.").with_in_deg(1).with_cond(has_no_lines),
+        else_body=~N("tail.").with_in_deg(1).with_cond(without_top_level_instructions("RERAISE", "END_FINALLY")).with_cond(has_no_lines),
         for_iter=N.tail().with_cond(with_instructions("FOR_ITER")),
         tail=N.tail(),
     )
@@ -96,7 +96,7 @@ class IfElseLoop(ControlFlowTemplate):
 class IfThen(ControlFlowTemplate):
     template = T(
         if_header=~N("if_body", "tail").with_cond(without_top_level_instructions("WITH_EXCEPT_START", "CHECK_EXC_MATCH", "FOR_ITER", "JUMP_IF_NOT_EXC_MATCH")),
-        if_body=~N(None).with_in_deg(1).of_type(BreakTemplate, ContinueTemplate) | ~N("tail").with_in_deg(1) | ~N("tail.").with_in_deg(1).with_cond(run_is(2)) | ~N.tail().with_in_deg(1).with_cond(exact_instructions("LOAD_CONST","RETURN_VALUE"), exact_instructions("POP_TOP", "LOAD_CONST","RETURN_VALUE")),
+        if_body=~N.tail().with_in_deg(1).of_type(BreakTemplate, ContinueTemplate) | ~N("tail").with_in_deg(1) | ~N("tail.").with_in_deg(1).with_cond(run_is(2)) | ~N.tail().with_in_deg(1).with_cond(exact_instructions("LOAD_CONST","RETURN_VALUE"), exact_instructions("POP_TOP", "LOAD_CONST","RETURN_VALUE")) | ~N.tail().with_in_deg(1).with_cond(ending_instructions("POP_TOP", "RERAISE")),
         tail=N.tail(),
     )
 
